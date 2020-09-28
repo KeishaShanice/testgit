d716252fab80df46cfe078d711885b6c4f3a9caf
KeishaShanice <shanice.lakeisha.sims@gmail.com>
2020-09-28 14:10:09 -0500

    wrote some awesome codez

--- js/app.js
+++ js/app.js
@@ -3,4 +3,42 @@
 // git init
 // git add --all
 // git commit -am "first commit"
-// 
+// |
+// -----Go to github and make remote repo?
+// |
+// -----Create branches??
+// git checkout -b staging (create branch)
+// Go to GH and create a new repo
+// Push an existing repository from the command line
+// git remote add origin git@github.com:KeishaShanice/testgit.git
+// git push -u origin --all
+
+// DO STUFF!!!
+// write code
+// commit the code
+// push the code
+// repeat
+
+
+// IF you add a file you need to ADD it to be tracked
+// git add --all
+// git commit -am "added <FILENAME>"
+// git push
+
+
+// Merge a feature back to master branch
+// get checkout master
+// git merge <BRANCHNAME> ie: git merge staging
+// git push
+// git checkout <BRANCHNAME>
+
+
+// Cloning a Repo
+// git clone git@github.com:KeishaShanice/testgit.git
+
+
+// git status
+// git branch -v
+// git remote -v
+
+
